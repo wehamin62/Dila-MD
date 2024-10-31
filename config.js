ad6372740fa12d5c83dac7014493cf8a3be23429
wehamin62 <cybrhash@gmail.com>
2024-10-31 18:34:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "huUiQQKZ#_XRye_XndY1CEe_0Xo57mrodv1nrSBw-Vk6qo4xgEf8",
 MONGODB: process.env.MONGODB || "",
 
 };
